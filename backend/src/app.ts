772395b14401a24fad8810d0f4f38c9ffb4669a9
sanskarOH <sanskaryeeha@gmail.com>
2026-04-27 03:51:06 +0530

    added prom

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,7 +4,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import { urlRouter } from "./routes/url.routes";
 import { errorHandler } from "./middleware/errorHandler";
-
+import client from "prom-client";
 export const app = express();
 
 app.use(helmet());
@@ -14,6 +14,11 @@ app.use(morgan("combined"));
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
+});
+
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.end(await client.register.metrics());
 });
 
 app.use("/", urlRouter);
